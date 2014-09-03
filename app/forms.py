6e3ead9b14fd75e3be40774a83536b72e3bbf222
Darshil Chanpura <dtchanpura@gmail.com>
2014-09-03 21:50:41 +0530

    Addition of Admin Page

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
-from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField, TextAreaField
+from flask.ext.wtf import Form #, RecaptchaField
+from wtforms import TextField, BooleanField, TextAreaField, PasswordField
 from wtforms.validators import Required, Length, ValidationError, Email
 from app.models import User
 from app import db
@@ -29,4 +29,24 @@ class RegistrationForm(Form):
 class AdminLogin(Form):
    username = TextField(validators = [Required()])
    password = PasswordField(validators = [Required()])
-   
+   '''def validate_on_submit(self):
+      if not db.session.query(User).filter_by(username = self.username.data).count():
+         return False
+      else:
+         return True'''
+   def getMetaData(self):
+      return db.session.query(User, Logs).\
+         filter(timestamp > datetime.timedelta(days = 1), 
+               timestamp <= datetime.datetime.today()).\
+               join(Logs).all()
+
+
+class ResendMail(Form):
+   email = TextField(validators = [Email()])
+   #recapt = RecaptchaField()
+   def validate_on_submit(self):
+      if not db.session.query(User).filter_by(email = self.email.data).count():
+         self.email.errors.append('Email not Registered. Check it again...')
+         return False
+      else:
+         return True
--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, redirect, url_for, request, flash
 from app import app, db, mail
 from flask.ext.mail import Message
-from forms import RegistrationForm
-from app.models import User
-from qrcode import QRCode, ERROR_CORRECT_H
+from forms import RegistrationForm, ResendMail
+from app.models import User, Logs
+from qrcode import QRCode, ERROR_CORRECT_H, ERROR_CORRECT_L
 from hashlib import sha1
 
 
@@ -50,3 +50,35 @@ def sendmail(mail_id):
    with app.open_resource("../qr.png") as fp:
       msg.attach("qr.png", "image/png", fp.read())
    mail.send(msg)
+
+@app.route('/resendmail',methods=['GET','POST'])
+def resendmail():
+   form = ResendMail(request.form)
+   if request.method == 'POST' and form.validate() and form.validate_on_submit():
+      qr = QRCode(version=10)
+      qr_data = sha1(form.email.data).hexdigest()
+      qr.add_data(qr_data)
+      qr.make() # Generate the QRCode itself
+      #im contains a PIL.Image.Image object
+      im = qr.make_image()
+      # To save it
+      im.save("qr_temp.png")
+      msg = Message("Your Attendance QR-Code.",
+               recipients=[form.email.data],
+               sender='cic3@gtu.ac.in',
+               html="<p>&nbsp;&nbsp;&nbsp;QR code has been re-generated please display it near the system while coming.<br><br> If you didn't requested for the new QR-Code please report it to CIC3 </p>"
+               )
+      with app.open_resource("../qr_temp.png") as fp:
+         msg.attach("qr_temp.png", "image/png", fp.read())
+      mail.send(msg)
+      return render_template('resend.html',issent=True)
+   return render_template('resend.html', form=form, issent=False)
+
+@app.route('/adminpanel')
+def adminpanel():
+   logs=Logs.query.\
+      filter(timestamp > datetime.timedelta(days = 2),
+         models.Logs.timestamp <= datetime.datetime.today()
+         ).all()
+
+   return render_template('admin.html',logs=logs)
